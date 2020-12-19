bb014c557be25af07237ca5084d8da5eda1c1780
MohammedKevin <kevin.mohammed16@gmail.com>
2020-12-19 22:42:49 +0100

    feat: :sparkles: add TodoItem and TodosList

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is my Todo Item</h1>
+      </div>
+    );
+  }
+}
+export default TodoItem;
---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is my TodosList Component</h1>
+      </div>
+    );
+  }
+}
