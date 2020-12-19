312578f76a43cd6a080e758a3a93e9f628249e7a
MohammedKevin <kevin.mohammed16@gmail.com>
2020-12-19 22:09:04 +0100

    feat: :sparkles: simple jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const names = ["Kevin", "Kamil", "Mohammed"];
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Reacht</h1>
+      <div>
+        <h2>Names</h2>
+        <ul className="names">
+          {names.map((name) => (
+            <li className="name">{name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
