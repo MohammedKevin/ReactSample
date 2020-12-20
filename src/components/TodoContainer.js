d58edf5c5226763d58d151e9ca60532b3d8f3531
MohammedKevin <kevin.mohammed16@gmail.com>
2020-12-20 13:06:16 +0100

    docs: :ambulance: temp change

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -23,6 +23,27 @@ class TodoContainer extends React.Component {
       },
     ],
   };
+  handleChange = (id) => {
+    //This code isnt how its supose to be done but its in there since the code below isnt working properly
+    this.setState({
+      todos: [
+        ...this.state.todos.map((todo) => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        }),
+      ],
+    });
+    // this.setState((prevState) => ({
+    //   todos: prevState.todos.map((todo) => {
+    //     if (todo.id === id) {
+    //       todo.completed = !todo.completed;
+    //     }
+    //     return todo;
+    //   }),
+    // }));
+  };
   delTodo = (id) => {
     this.setState({
       todos: [
@@ -55,16 +76,5 @@ class TodoContainer extends React.Component {
       </div>
     );
   }
-  handleChange = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id == id) {
-          todo.completed = !todo.completed;
-          console.log(id);
-        }
-        return todo;
-      }),
-    }));
-  };
 }
 export default TodoContainer;
